f8b97d2f8c5267c13ec2a1af541125f4b76f0182
Alex Dergachev <alex@evolvingweb.ca>
2012-06-17 23:45:03 -0400

    deleting files after move into chrom-xpath-link subdirectory

--- menu.js
+++
@@ -1,7 +0,0 @@
-chrome.contextMenus.create({
-    title: "Get link to XPath",
-    "contexts": ["all"],
-    onclick: function(info, tab) {
-        chrome.tabs.sendRequest(tab.id, {name: "xpath-link_contextMenuClick"});  
-    }
-});
